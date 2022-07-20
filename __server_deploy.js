a4ed487cf55b45e8c9f124fb42c582db34d68387
Faisal Ahmed <faisal2410@yahoo.com>
2022-07-20 16:25:09 +0600

    registration updated

--- __server_deploy.js
+++ __server_deploy.js
@@ -11,7 +11,14 @@
  * 1.Heroku Create
  * 2. make sure you add . commit and push to git
  * 3. git push heroku master
- * 
- * 
+ * 4. Go to heroku>dashboard>current project>settings>Reveal config bars>
+ * 5.copy paste config vars from env file
+ * 6. Make sure you have whitelisted all ip address to access mongodb
+ * ...........................................................................
+ * Update Server with new changes
+ * ....................................................................
+ * 1. Make Changes
+ * 2.make sure you add . commit and push to git
+ * 3. git push heroku master
  * 
 */
--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -6,7 +6,7 @@ exports.Registration=(req,res)=>{
 let reqBody=req.body;
 UsersModel.create(reqBody,(err,data)=>{
     if(err){
-        res.status(400).json({status:"Fail",data:err});
+        res.status(200).json({status:"Fail",data:err});
     }else{
         res.status(200).json({status:"Success",data:data})
     }
